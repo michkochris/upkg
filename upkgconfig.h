db82fcc46b942c29e81f2d8f63a802ea94d188b3
michkochris <michkochris@gmail.com>
2025-01-08 18:41:03 -0500

    updated comment headers

--- upkgconfig.h
+++ upkgconfig.h
@@ -1,3 +1,25 @@
+/******************************************************************************
+ *  Filename:    upkgconfig.h
+ *  Author:      <michkochris@gmail.com>
+ *  Date:        started0 12-31-2024
+ *  Description: upkg manages linux .deb pkg's
+ *
+ *  Copyright (c) 2025 upkg (ulinux) All rights reserved.
+ *  GPLV3
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program. If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/*file description: header file to export upkgconfig.c's functions*/
 #ifndef UPKGCONFIG_H
 #define UPKGCONFIG_H
 char *get_config_value(const char *filename, const char *key);
